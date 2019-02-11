5f08cb0930ec1093b323ad1cb0e00978f5a1c23a
soulle <saulekazanbaeva3110@gmail.com>
2019-02-11 15:08:30 +0300

    test commit

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+
+const program = require('commander');
+
+program
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-h, --help', 'output usage information')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+
+program.on('--help', function(){
+  console.log('')
+})
+  
+program.parse(process.argv);
+
+program.help();
