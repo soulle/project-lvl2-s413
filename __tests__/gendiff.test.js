d45fd4a5450d16a617b4a6b217273a0485d67d05
soulle <saulekazanbaeva3110@gmail.com>
2019-02-12 15:10:59 +0300

    fix gendiff test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import genDiff from '../src';
 
 test('genDiff(pathToFile1, pathToFile2) is equal expected_step2', () => {
-  const expected = fs.readFileSync('/home/saule/gendiff/__tests__/__fixtures__/expected_step2', 'utf-8');
-  const received = genDiff('/home/saule/gendiff/__tests__/__fixtures__/before.json', '/home/saule/gendiff/__tests__/__fixtures__/after.json');
+  const expected = fs.readFileSync('__tests__/__fixtures__/expected_step2', 'utf-8');
+  const received = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
   expect(received).toEqual(expected);
 });
