2706f730f2013860c1169a9a37704432915de197
soulle <saulekazanbaeva3110@gmail.com>
2019-02-12 16:20:45 +0300

    fix program

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,12 +1,11 @@
 #!/usr/bin/env node
-
-import program from 'commander';
 import generator from '..';
+import program from 'commander';
 
 export default program
   .version('1.0.2')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(generator)
+  .action((firstConfig, secondConfig) => console.log(generator(firstConfig, secondConfig)))
   .parse(process.argv);
