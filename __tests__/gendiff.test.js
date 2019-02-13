7f704220b65c0a1b021a9f42aa41e01faef4c1d2
soulle <saulekazanbaeva3110@gmail.com>
2019-02-13 14:45:10 +0300

    add step4

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,10 +1,11 @@
 import fs from 'fs';
 import genDiff from '../src';
 
-test('genDiff with flat files', () => {
-  const expected = fs.readFileSync('__tests__/__fixtures__/expected_step2', 'utf-8');
-  const actualJson = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
-  expect(actualJson).toEqual(expected);
-  const actualYaml = genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml');
-  expect(actualYaml).toEqual(expected);
-});
+const expectedResult = fs.readFileSync('__tests__/__fixtures__/expected_step2', 'utf-8');
+const actual0 = genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json');
+const actual1 = genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml');
+const actual2 = genDiff('__tests__/__fixtures__/before.ini', '__tests__/__fixtures__/after.ini');
+test.each([[actual0, expectedResult], [actual1, expectedResult], [actual2, expectedResult]])(
+  'genDiff %#',
+  (actual, expected) => expect(actual).toEqual(expected),
+);
--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,10 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsing = {
   '.json': data => JSON.parse(data),
   '.yml': data => yaml.safeLoad(data),
+  '.ini': data => ini.parse(data),
 };
 
 const parse = (data, ext) => parsing[ext](data);
